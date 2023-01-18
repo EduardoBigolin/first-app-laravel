8845e2770e6ca91db06cad6193f4348e7e8e5019
Eduardo Bigolin <93228914+SrDivergente@users.noreply.github.com>
2023-01-18 19:53:12 -0300

    create home page and header

---
+++ resources/views/site/home/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+
+<body class="bg-slate-900">
+    <header class="bg-slate-800 p-5 flex items-center justify-between text-white">
+        <h1 class="text-2xl font-bold">MoviesSi</h1>
+        <ul class="flex gap-5 text-[#9e9e9e]">
+            <li class="hover:text-white"><a href="#">Home</a></li>
+            <li class="hover:text-white"><a href="#">Create new MOVIE</a></li>
+            <li class="hover:text-white"><a href="#">Contact</a></li>
+            <li class="hover:text-white"><a href="#">About</a></li>
+        </ul>
+        <div class="flex items-center">
+            <input type="text" class="p-1 rounded-l-sm text-[#222]" placeholder="Search...">
+            <button class="bg-slate-500 p-1 rounded-r-sm">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+                </svg>
+            </button>
+        </div>
+    </header>
+    <main>
+        <!-- <div class="w-40 h-60 text-white text-center">
+            <img class="" src="https://m.media-amazon.com/images/M/MV5BOWNlMTJmMWUtYjk0MC00M2U4LWI1ODItZDgxNDZiODFmNjc5XkEyXkFqcGdeQXVyMTAwMzUyOTc@._V1_.jpg" alt="" />
+            <h4 class="font-bold">Title Movie</h4>
+            <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div> -->
+
+    </main>
+</body>
+
+</html>
